176074d4a7933f6ec65ea838932cdcf9a678a356
Jens Marchewka <im.naice@googlemail.com>
2023-11-05 18:57:37 +0100

    fixed routing

--- Program.cs
+++ Program.cs
@@ -9,9 +9,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-var baseUri = new Uri("https://naice.github.io/Arkanium/");
-//baseUri = new Uri("https://localhost:5001");
-
 var services = builder.Services;
 services.AddScoped<IClipboardService, ClipboardService>();
 services.AddHttpClient("Arkanium.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
@@ -19,15 +16,11 @@ services.AddHttpClient("Arkanium.ServerAPI", client => client.BaseAddress = new 
 // Supply HttpClient instances that include access tokens when making requests to the server project
 services.AddScoped(sp => {
     var client = sp.GetRequiredService<IHttpClientFactory>().CreateClient("Arkanium.ServerAPI");
-    client.BaseAddress = baseUri;
     return client;
 });
 
 services.AddRadzenComponents();
 
 var host = builder.Build();
-
-// var navMan = host.Services.GetRequiredService<NavigationManager>();
-// navMan.BaseUri = baseUri;
 
 await host.RunAsync();
